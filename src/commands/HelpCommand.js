542aa81865c08e17378e51d0bef7f341df75b81d
x4nk1t <ankit.maharjan.252@gmail.com>
2020-06-28 14:28:56 +0545

    [Help Command] Added pages

--- src/commands/HelpCommand.js
+++ src/commands/HelpCommand.js
@@ -4,28 +4,48 @@ const EmbedBuilder = require('../utils/EmbedBuilder.js')
 class HelpCommand{
     constructor(commandLoader){
         this.commandLoader = commandLoader;
-        this.usage = this.commandLoader.prefix +"help";
+        this.usage = this.commandLoader.prefix +"help [page]";
         this.description = "Shows the command info.";
         
-        this.helpContent = "";
+        this.helpContent = [];
+        this.helpPerPage = 4;
     }
     
     onCommand(message, commandArgs){
         message.channel.startTyping()
         if(this.helpContent == ""){
             var commands = this.commandLoader.loadedCommands;
+            var page = 0;
             
             for(var i = 0; i < commands.length; i++){
+                if(i != 0 && i % this.helpPerPage == 0) page++;
+                if(this.helpContent[page] == null) this.helpContent[page] = '';
+                
                 const commandName = this.commandLoader.prefix + commands[i].commandName;
                 const usage = commands[i].getCommandClass().usage;
                 const commandDescription = commands[i].getCommandClass().description;
                 
-                this.helpContent += '**'+ commandName + "**\n" + commandDescription + '\n Usage: ' + usage +'\n';
+                this.helpContent[page] += '**'+ commandName + "**\n" + commandDescription + '\n Usage: ' + usage +'\n';
             }
         }
+        var reqPage = 0;
+        if(commandArgs[0]){
+            reqPage = commandArgs[0] - 1;
+            if(commandArgs[0] > this.helpContent.length){
+                var embed = new discord.MessageEmbed()
+                    .setColor('#FF0000')
+                    .setTitle('Error')
+                    .setDescription('That page is not available. Total Available Page: '+ this.helpContent.length)
+                
+                message.channel.stopTyping()
+                message.channel.send(embed)
+                return;
+            }
+        }
+        
         var embed = new EmbedBuilder().build()
-            .setTitle('-= Help =-')
-            .setDescription(this.helpContent)
+            .setTitle('-= Help (Page '+ (reqPage + 1) + ') =-')
+            .setDescription(this.helpContent[reqPage])
             .setFooter('Requested by '+ message.author.username, message.author.displayAvatarURL())
             .setTimestamp()
         
