7da4da7215f219b5d3fd2dd41646919dae70ac5d
x4nk1t <47915404+x4nk1t@users.noreply.github.com>
2020-09-11 15:13:14 +0545

    bug fix and removed logs

--- src/utils/RPGReminder.js
+++ src/utils/RPGReminder.js
@@ -17,16 +17,13 @@ class RPGReminder {
         var sc = args[0]
         
         if(sc == "hunt"){
-            console.log('hunt received')
             if(!this.hunt.has(message.author.id)){
                 this.hunt.set(message.author.id, {time: now + 60000, message: message}) //1min
             }
         }
         
         if(sc == "adv" || sc == "adventure"){
-            console.log('adv received')
             if(!this.adventure.has(message.author.id)){
-                console.log('adv added')
                 this.adventure.set(message.author.id, {time: now + 3600000, message: message}) //1hr
             }
         }
@@ -42,16 +39,14 @@ class RPGReminder {
            sc == "pickup" || sc == "ladder" ||
            sc == "mine" || sc == "bowsaw" ||
            sc == "boat" || sc == "pickaxe"){
-            console.log('progess received')
             if(!this.progress.has(message.author.id)){
-                console.log('progress added')
                 this.progress.set(message.author.id, {time: now + 300000, message: message}) //5min
             }
         }
     }
     
     run(){
-        setInterval(() => this.checkReminders, 1000)
+        setInterval(() => this.checkReminders(), 1000)
     }
     
     checkReminders(){
@@ -66,7 +61,6 @@ class RPGReminder {
             var message = value.message;
             
             if((time - now) <= 0){
-                console.log('adv sent')
                 message.channel.send('Hunt Ready! <@'+id+'>').then(() => {
                     this.hunt.delete(id)
                 })
@@ -79,7 +73,6 @@ class RPGReminder {
             var message = value.message;
             
             if((time - now) <= 0){
-                console.log('adv sent')
                 message.channel.send('Adventure Ready! <@'+id+'>').then(() => {
                     this.adventure.delete(id)
                 })
@@ -92,7 +85,6 @@ class RPGReminder {
             var message = value.message;
             
             if((time - now) <= 0){
-                console.log('progress sent')
                 message.channel.send('Progress Ready! <@'+id+'>').then(() => {
                     this.progress.delete(id)
                 })
