d8cce707e6df11b35ac56f0663794ef682fc4364
x4nk1t <47915404+x4nk1t@users.noreply.github.com>
2020-09-11 16:01:18 +0545

    test 3

--- src/utils/RPGReminder.js
+++ src/utils/RPGReminder.js
@@ -21,20 +21,20 @@ class RPGReminder {
         }
         
         if(sc == "hunt"){
-            if(!this.hunt.has(message.author.id.toString())){
-                this.hunt.set(message.author.id.toString(), {time: now + 60000, message: message}) //1min
+            if(!this.hunt.has(message.author.id)){
+                this.hunt.set(message.author.id, {time: now + 60000, message: message}) //1min
             }
         }
         
         if(sc == "adv" || sc == "adventure"){
-            if(!this.adventure.has(message.author.id.toString())){
-                this.adventure.set(message.author.id.toString(), {time: now + 3600000, message: message}) //1hr
+            if(!this.adventure.has(message.author.id)){
+                this.adventure.set(message.author.id, {time: now + 3600000, message: message}) //1hr
             }
         }
         
         if(sc == "training"){
-            if(!this.training.has(message.author.id.toString())){
-                this.training.set(message.author.id.toString(), {time: now + 900000, message: message}) //15min
+            if(!this.training.has(message.author.id)){
+                this.training.set(message.author.id, {time: now + 900000, message: message}) //15min
             }
         }
         
@@ -43,8 +43,8 @@ class RPGReminder {
            sc == "pickup" || sc == "ladder" ||
            sc == "mine" || sc == "bowsaw" ||
            sc == "boat" || sc == "pickaxe"){
-            if(!this.progress.has(message.author.id.toString())){
-                this.progress.set(message.author.id.toString(), {time: now + 300000, message: message}) //5min
+            if(!this.progress.has(message.author.id)){
+                this.progress.set(message.author.id, {time: now + 300000, message: message}) //5min
             }
         }
     }
