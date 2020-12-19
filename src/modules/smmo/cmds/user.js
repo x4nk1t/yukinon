2efeeb45d7bc782b9e47d4c0b277f6f9499e0bed
x4nk1t <47915404+x4nk1t@users.noreply.github.com>
2020-12-19 16:24:17 +0545

    api changes fix and some other changes

--- src/modules/smmo/cmds/user.js
+++ src/modules/smmo/cmds/user.js
@@ -21,29 +21,29 @@ class Volume extends Command{
                     .then(response => {
                         const data = response.data;
                         
-                        if(data == 'user not found'){
+                        if(data.error == 'user not found'){
                             message.channel.send({embed: {color: 'BLUE', description: 'User not found.'}})
                             return
                         }
 
-                        const profile_number = data.profile_number != null ? data.profile_number : '';
-                        const guild = data.guild ? `[${data.guild.name}](https://web.simple-mmo.com/guild/view/${data.guild.id})` : 'None';
+                        const profile_number = data.profile_number != null ? "#"+ data.profile_number : '';
+                        const guild = data.guild ? `[${data.guild.name}](https://web.simple-mmo.com/guilds/view/${data.guild.id})` : 'None';
 
                         const embed = {
                             title: data.name + profile_number,
                             color: 'BLUE',
                             url: 'https://web.simple-mmo.com/user/view/'+ id,
-                            description: `[[Attack]](https://web.simple-mmo.com/user/attack/${id}) [[Message]](https://web.simple-mmo.com/messages/view/user/${id}) [[Send Gold]](https://web.simple-mmo.com/sendgold/${id}) [[Send Item]](https://web.simple-mmo.com/inventory?sendid=${id})`,
+                            description: `*${data.motto}*\n[[Attack]](https://web.simple-mmo.com/user/attack/${id}) [[Message]](https://web.simple-mmo.com/messages/view/user/${id}) [[Send Gold]](https://web.simple-mmo.com/sendgold/${id}) [[Send Item]](https://web.simple-mmo.com/inventory?sendid=${id})`,
                             fields: [
-                                {name: 'Level', value: data.level, inline: true},
-                                {name: 'Gold', value: data.gold, inline: true},
-                                {name: 'Steps', value: data.steps, inline: true},
-                                {name: 'Strength', value: data.str + ` (+${data.bonus_str})`, inline: true},
-                                {name: 'Defence', value: data.def + ` (+${data.bonus_def})`, inline: true},
-                                {name: 'Dexterity', value: data.dex + ` (+${data.bonus_dex})`, inline: true},
-                                {name: 'User Kills', value: data.user_kills, inline: true},
-                                {name: 'NPC Kills', value: data.npc_kills, inline: true},
-                                {name: 'Quests Completed', value: data.quests_complete, inline: true},
+                                {name: 'Level', value: data.level.toLocaleString(), inline: true},
+                                {name: 'Gold', value: data.gold.toLocaleString(), inline: true},
+                                {name: 'Steps', value: data.steps.toLocaleString(), inline: true},
+                                {name: 'Strength', value: data.str.toLocaleString() + ` (+${data.bonus_str.toLocaleString()})`, inline: true},
+                                {name: 'Defence', value: data.def.toLocaleString() + ` (+${data.bonus_def.toLocaleString()})`, inline: true},
+                                {name: 'Dexterity', value: data.dex.toLocaleString() + ` (+${data.bonus_dex.toLocaleString()})`, inline: true},
+                                {name: 'User Kills', value: data.user_kills.toLocaleString(), inline: true},
+                                {name: 'NPC Kills', value: data.npc_kills.toLocaleString(), inline: true},
+                                {name: 'Quests Completed', value: data.quests_complete.toLocaleString(), inline: true},
                                 {name: 'Safe mode' , value: data.safeMode == 0 ? "No" : "Yes", inline: true},
                                 {name: 'Guild', value: guild, inline: true},
                             ],
@@ -51,6 +51,9 @@ class Volume extends Command{
                                 text: 'Requested by '+ message.author.username,
                                 icon_url: message.author.displayAvatarURL() 
                             }
+                        }
+                        if(data.safeModeTime){
+                            embed.fields.push({name: 'Safe Mode Time <:idk:769065058788442112>', value: data.safeModeTime, inline: true})
                         }
                         message.channel.send({embed: embed})
                     }).catch(err => {
