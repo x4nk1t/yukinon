9c3272d8c40c96590381cb4e4238f6c4371bc880
x4nk1t <47915404+x4nk1t@users.noreply.github.com>
2021-01-17 12:24:21 +0545

    react animated emojis to last message

--- src/modules/emoji/EmojiSender.js
+++ src/modules/emoji/EmojiSender.js
@@ -9,6 +9,32 @@ class EmojiSender {
             
             var content = message.content;
             var emojis = message.guild.emojis.cache;
+            var suffix = '__react';
+
+            if(content.toLowerCase().endsWith(suffix)){
+                const emojiName = content.slice(0, (suffix.length * -1))
+
+                emojis.forEach(async emoji => {
+                    if(emojiName.toLowerCase() == emoji.name.toLowerCase()){
+                        if(emoji.animated){
+                            const messages = await message.channel.messages.fetch({limit: 2})
+                            const lastMessage = messages.last()
+                            if(lastMessage){
+                                lastMessage.react(emoji)
+                                const reactionCollector = lastMessage.createReactionCollector((reaction, user) => reaction.emoji.name == emoji.name && !user.bot, { time: 30000 })
+                                reactionCollector.on('collect', reaction => {
+                                    reaction.users.remove(this.client.user)
+                                    reactionCollector.stop()
+                                })
+                            }
+                            message.delete()
+                            return
+                        }
+                    }
+                })
+                return
+            }
+
             emojis.some(emoji => {
                 if(content.toLowerCase() == emoji.name.toLowerCase()){
                     var animated = ''
