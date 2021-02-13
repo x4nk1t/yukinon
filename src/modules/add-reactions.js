aedf370db64247c8e269cc1c56323a896d631c33
x4nk1t <47915404+x4nk1t@users.noreply.github.com>
2021-02-13 22:31:02 +0545

    some improvements to add reactions

--- src/modules/add-reactions.js
+++ src/modules/add-reactions.js
@@ -8,17 +8,15 @@ class AddReactions {
     registerEvent(){
         this.client.on('message', message => {
             if(message.author.id != '432610292342587392') return
+            if(message.embeds.length <= 0) return
+            if(!message.embeds[0].description.includes('<:kakera:469835869059153940>')) return
 
             const now = new Date().getTime()
             const startTime = new Date(new Date().setUTCHours(14,0,0,0)).getTime()
             const endTime = new Date(new Date().setUTCHours(31,0,0,0)).getTime()
 
             if((startTime - now) <= 0 && (endTime - now) >= 0){
-                if(message.embeds.length){
-                    if(message.embeds[0].description.includes('<:kakera:469835869059153940>')){
-                        message.react('❤️')
-                    }
-                }
+                message.react('❤️')
             }
         })
     }
