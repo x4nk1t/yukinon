d5f31c83124e4b68d919445997f5485c1d562d0d
x4nk1t <47915404+x4nk1t@users.noreply.github.com>
2021-02-14 13:05:35 +0545

    add reactions update

--- src/modules/add-reactions.js
+++ src/modules/add-reactions.js
@@ -15,7 +15,7 @@ class AddReactions {
             const startTime = new Date(new Date().setUTCHours(14,0,0,0)).getTime()
             const endTime = new Date(new Date().setUTCHours(31,0,0,0)).getTime()
 
-            if((startTime - now) <= 0 || (endTime - now) >= 0){
+            if((now > startTime) && (now < endTime)){
                 message.react('❤️')
             }
         })
