b595c4267a4d7be0ea0608bda22a765f1ae7443a
x4nk1t <47915404+x4nk1t@users.noreply.github.com>
2021-03-15 12:22:22 +0545

    time changed and now only react if there are no other reactions

--- src/modules/add-reactions.js
+++ src/modules/add-reactions.js
@@ -9,25 +9,27 @@ class AddReactions {
         this.client.on('message', message => {
             if(message.author.id != '432610292342587392') return
             if(message.embeds.length <= 0) return
-            
-            const embed = message.embeds[0];
-            const description = embed.description.toLowerCase();
-            if(!description.includes('<:kakera:469835869059153940>') || description.includes('like') || embed.image == null) return
-            
-            const now = new Date()
-            const startDate = new Date(new Date().setUTCHours(14,0,0,0))
-            const endDate = new Date(new Date().setUTCHours(31,0,0,0))
 
-            if(now.getUTCHours() < endDate.getUTCHours()){
-                startDate.setUTCDate(now.getUTCDate() - 1)
-            }
+            setTimeout(() => {
+                const embed = message.embeds[0];
+                const description = embed.description.toLowerCase();
+                if(!description.includes('<:kakera:469835869059153940>') || description.includes('like') || embed.image == null || message.reactions.cache.size > 0) return
 
-            const startTime = startDate.getTime()
-            const endTime = endDate.getTime()
-            
-            if((now > startTime) && (now < endTime)){
-                message.react('❤️')
-            }
+                const now = new Date()
+                const startDate = new Date(new Date().setUTCHours(12,0,0,0))
+                const endDate = new Date(new Date().setUTCHours(31,0,0,0))
+
+                if(now.getUTCHours() < endDate.getUTCHours()){
+                    startDate.setUTCDate(now.getUTCDate() - 1)
+                }
+
+                const startTime = startDate.getTime()
+                const endTime = endDate.getTime()
+                
+                if((now > startTime) && (now < endTime)){
+                    message.react('❤️')
+                }
+            }, 500)
         })
     }
 }
