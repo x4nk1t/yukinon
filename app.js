819e481feb7332d9f23153495581f2444fc1f10a
x4nk1t <ankit.maharjan.252@gmail.com>
2022-04-11 21:20:23 +0545

    fix joinleave not working

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-const { Intents } = require('discord.js');
 const Client = require("./src/Client.js");
-const client = new Client({intents: ['GUILDS', 'DIRECT_MESSAGES', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS']})
+const client = new Client({intents: ['GUILDS', 'DIRECT_MESSAGES', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS', 'GUILD_MEMBERS']})
 
 client.login(process.env.BOT_TOKEN)
